6f39b7784be5963d34d74f070fbeba22a1157240
qizles <qizles@gmail.com>
2018-01-27 15:33:48 +0100

    added allowed host

--- mysite/mysite/settings/dev.py
+++ mysite/mysite/settings/dev.py
@@ -5,7 +5,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.53.31", "qizl.es", "localhost", "cms.qizl.es", "52.59.215.159", "/run/gunicorn/socket"]
+ALLOWED_HOSTS = ["192.168.53.31", "qizl.es", "localhost", "cms.qizl.es", "52.59.215.159", "/run/gunicorn/socket", "*"]
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '-bwr19^)+1hqj3j9nmtc_i*_jer4%+%foscnhbhvlg6h0ycgj)'
