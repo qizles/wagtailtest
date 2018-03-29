4bdf6be791522a9b48c2aba3d6cbbefb5508a40e
qizles <qizles@gmail.com>
2018-03-29 19:09:38 +0200

    added sidenav

--- mysite/mysite/static/js/init.js
+++ mysite/mysite/static/js/init.js
@@ -3,6 +3,23 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('#columniwannapush').pushpin({
+        top: $('#columniwannapush').offset().top,
+        //bottom: columniwannapush.offset().top + columniwannapush.outerHeight() - columniwannapush.height()
+    });
+
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+
+
+
+ //$(document).ready(function(){
+//    $('.pushpin').pushpin({
+//      top: 1000,
+//      offset: 400
+//    });
+//  });
